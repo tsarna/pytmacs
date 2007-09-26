2434ea9527ac138efae4acfcd9875da4e4530f27
Ty Sarna <ty@sarna.org>
2007-09-26 03:59:07 +0000

    Checkpoint my lorem Ipsum generator, to be turned into a command at some point. Also useful for tests.
    git-svn-id: file:///Users/tsarna/src/SVNREPO/tmacs/trunk@343 933abd37-16e6-440f-98ae-168aefc19175

---
+++ tmacs/addons/lorem.py
@@ -0,0 +1,103 @@
+# The literary works of Etaoin Shrdlu
+
+"""Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+mollit anim id est laborum."""
+
+# This is some of the rest of the text that Lorem Ipsum was taken from
+# we don't generate this, but use it to enrich the probabilities table.
+
+_more = """Sed ut perspiciatis, unde omnis iste natus error sit
+voluptatem accusantium doloremque laudantium, totam rem aperiam eaque
+ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae
+dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit,
+aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos,
+qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui
+doquia quia non numquam quaerat voluptatem. emporis suscipit, vel eum
+qui ea, quam nihil molestiae consequatur, vel, qui, quo voluptas? At
+vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis
+praesentium voluptatum deleniti atque corrupti, quos dolores et quas
+molestias, similique et dolorum fuga. Et harum quidem rerum facilis est
+et expedita distinctio. Nam libero tempore, cum soluta nobis est
+eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat,
+facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.
+Temporibus autem quibusdam et aut officiis debitis aut rerum
+necessitatibus saepe eveniet, ut et voluptates repudiandae sint et
+molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente
+delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut
+perferendis doloribus asperiores repellat."""
+
+
+from random import choice
+
+# build probabilities data
+
+def _mkprobs(s, probs, starts):
+    f = s[0].lower()
+    starts.append(f)
+    probs = {}
+
+    last = ' '  + f
+    for c in s[1:] + ' ':
+        if c == '\n':
+            c = ' '
+        else:
+            c = c.lower()
+        probs[last] = probs.get(last, "") + c.lower()
+        if last[1] == ' ':
+            starts.append(c)
+        last = last[1]+c
+
+    return probs, starts
+
+
+_probs, _starts = _mkprobs(__doc__, {}, [])
+_probs, _starts = _mkprobs(_more, _probs, _starts)
+    
+
+def genword():
+    l = [choice(_starts)]
+    last = ' '+l[0]
+    while True:
+        # as words get longer, stack the odds of ending
+        tries = max((len(l) - 7) // 5, 0) + 1
+        for x in range(tries):
+            c = choice(_probs[last])
+            if c == ' ':
+                return ''.join(l)
+
+        if len(l) > 12:
+            c = choice(_probs[last])
+            if c == ' ':
+                return ''.join(l)
+        l.append(c)
+        last = last[1] + c
+
+
+def loremwords():
+    l = __doc__.split()
+    for w in l:
+        yield w
+
+    start = True
+
+    last = None
+    while True:
+        w = genword()
+        if w == last:
+            continue
+
+        last = w            
+        if start:
+            w = w.title()
+        start = (w[-1] in '.?')
+
+        yield w
+
+l = loremwords()
+
+print ' '.join([l.next() for x in range(2100)])
